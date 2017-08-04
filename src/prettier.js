08786d9834d137c9a205048fd703c3722f522b07
Dustin Schau <dustinschau@gmail.com>
2017-08-04 01:50:49 -0500

    chore: minor tweaks

--- src/prettier.js
+++ src/prettier.js
@@ -29,7 +29,12 @@ fetch(`${ENDPOINT}?url=${url}`, {
     'Content-Type': 'application/json'
   }
 })
-  .then(response => response.json())
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
   .then(data => {
     const { blocks } = data;
     if (blocks.length === tags.length) {
